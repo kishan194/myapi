4edabdfa012d5fdf929c5498705158011acef109
Kishan Baraiya <kishanbaraiya2002@gmail.com>
2024-03-06 12:38:51 +0530

    wrong code

--- app/Http/Controllers/MakeApiController.php
+++ app/Http/Controllers/MakeApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Console\Commands\makeApi;
+use App\Models\test;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 
@@ -10,7 +11,8 @@ class MakeApiController extends Controller
 {
     public function makeApi(){
         Artisan::call('call:api');
-        return response()->json(['message' => 'API called successfully']);
+        return  test::all();
+        
     }
 
     public function data(){
